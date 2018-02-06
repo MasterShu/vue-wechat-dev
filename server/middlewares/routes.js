fb021b6f999afaf482993442915d3cd04300b3b7
MasterShu <1562688988@qq.com>
2018-02-06 23:13:40 +0800

    init project

---
+++ server/middlewares/routes.js
@@ -0,0 +1,15 @@
+import Router from 'koa-router'
+
+export const routes = app => {
+  const router = new Router()
+
+  router.get('/wechat-hear', (ctx, next) => {
+
+  })
+  router.post('/wechat-hear', (ctx, next) => {
+
+  })
+
+  app.use(router.routes())
+  app.use(router.allowMethods())
+}
